204b8c01d1b37cbfb068969316ef71eb2a102415
KuangjuX <qcx@tju.edu.cn>
2021-09-06 07:59:49 -0700

    update

--- kvs/src/error.rs
+++ kvs/src/error.rs
@@ -4,7 +4,10 @@ use std::io;
 #[derive(Fail, Debug)]
 pub enum Error {
     #[fail(display = "{}", _0)]
-    IOErr(#[cause] io::Error)
+    IOErr(#[cause] io::Error),
+
+    // #[fail(display = "{}", _0)]
+    // StdErr(#[cause] &'static str)
 }
 
 impl From<io::Error> for Error {
@@ -12,5 +15,11 @@ impl From<io::Error> for Error {
         Error::IOErr(err)
     }
 }
+
+// impl From<&'static str> for Error {
+//     fn from(err: &'static str) -> Error {
+//         Error::StdErr(err)
+//     }
+// }
 
 pub type Result<T> = std::result::Result<T, Error>;
--- kvs/src/kv.rs
+++ kvs/src/kv.rs
@@ -1,10 +1,13 @@
 use std::collections::HashMap;
 use std::path::PathBuf;
 use std::fs::{ File, OpenOptions };
-use std::io::{ Seek, SeekFrom, Read, Write };
-use serde::{Serialize, Deserialize};
+use std::io::{ Seek, SeekFrom, Read, Write, Error };
+use std::str;
+use std::sync::{ Mutex, Arc };
 
-use std::sync::RwLock;
+use serde::{Serialize, Deserialize};
+use serde_json;
+
 
 use super::Result;
 
@@ -13,66 +16,92 @@ pub trait Disk {
     fn write(&self, offset: usize, buf: &[u8]);
 }
 
-pub struct BlockFile(RwLock<File>);
+pub struct BlockFile(Mutex<File>);
 
 impl Disk for BlockFile {
     fn read(&self, offset: usize, buf: &mut [u8]) {
-        let mut file = self.0.read().unwrap();
+        let mut file = self.0.lock().unwrap();
         file.seek(SeekFrom::Start(offset as u64)).expect("Error when seeking!");
         assert_eq!(file.read(buf).unwrap(), buf.len());
     }
 
     fn write(&self, offset: usize, buf: &[u8]) {
-        let mut file = self.0.write().unwrap();
+        let mut file = self.0.lock().unwrap();
         file.seek(SeekFrom::Start(offset as u64)).expect("Error when seeking!");
         assert_eq!(file.write(buf).unwrap(), buf.len());
     }
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-pub struct KvDate {
-    key: String,
-    value: String
+pub struct MemoryData {
+    offset: usize, 
+    len: usize
 }
 
-
-
 pub struct KvStore {
-    // map: HashMap<String, String>,
-    disk: BlockFile
+    /// 键以及偏移量
+    map: HashMap<String, MemoryData>,
+    /// 块设备
+    device: Arc<BlockFile>,
+    /// 磁盘大小
+    size: usize
 }
 
 impl KvStore {
     /// Create a KvStore.
     pub fn new() -> Self {
         Self {
-            disk: BlockFile(
-                RwLock::new({
-                    let f = OpenOptions::new()
-                        .read(true)
-                        .write(true)
-                        .create(true)
-                        .open("disk")
-                        .unwrap();
-                    f
-                })
-            )
+            map: HashMap::new(),
+            device: Arc::new(
+                BlockFile(
+                    Mutex::new({
+                        let f = OpenOptions::new()
+                            .read(true)
+                            .write(true)
+                            .create(true)
+                            .open("disk")
+                            .unwrap();
+                        f
+                    })
+                )
+            ),
+            size: 0,
         }
     }
 
     /// Get value by key.
     pub fn get(&self, key: String) -> Result<Option<String>> {
-        // match self.map.get(&key) {
-        //     Some(value) => Ok(Some(value.clone())),
+        match self.map.get(&key) {
+            Some(memory_data) => {
+                let offset = memory_data.offset;
+                let len = memory_data.len;
+                let mut buf = vec![0; len];
+                let device = Arc::clone(&self.device);
+                device.read(offset, &mut buf);
 
-        //     None => Ok(None),
-        // }
-        unimplemented!()
+                let data: Command = serde_json::from_str(str::from_utf8(&buf).unwrap()).unwrap();
+                match data {
+                    Command::Set(_, value) => {
+                        Ok(Some(value))
+                    },
+
+                    _ => {
+                        Ok(None)
+                    }
+                }
+            },
+
+            None => {
+                Ok(None)
+            }
+        }
     }
 
     /// Set key-value
     pub fn set(&mut self, key: String, value: String) -> Result<()> {
-        
+        // match self.map.get(&key) {
+          
+        // }
+        unimplemented!()
     }
 
     /// Remove key
@@ -83,4 +112,10 @@ impl KvStore {
     pub fn open(path: impl Into<PathBuf>) -> Result<Self> {
         unimplemented!()
     }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum Command {
+    Set(String, String),
+    Remove(String)
 }
